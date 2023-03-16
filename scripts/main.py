2cc9ee48e35902c57bb81cd1d8fe9411768a0e03
wgall <wojtekgall2003@gmail.com>
2023-03-16 23:20:52 +0100

    Removed credentials injection

--- scripts/main.py
+++ scripts/main.py
@@ -7,13 +7,11 @@ project = sys.argv[1]
 from functions import is_bucket_present
 
 is_present = is_bucket_present(project.strip())
-print(is_present);
 
+if is_present == False: 
+    os.system("./bucket.sh {}".format(project))
+if is_present == True:
+    os.system("gcloud storage rm gs://{}/source_code.zip".format(project))
 
-# if is_present == False: 
-#     os.system("./bucket.sh {}".format(project))
-# if is_present == True:
-#     os.system("gcloud storage rm gs://{}/source_code.zip".format(project))
-
-# os.system("./deploy.sh {}".format(project))
+os.system("./deploy.sh {}".format(project))
     
