2ca0d4b12a73ff38d5cd9ace40494827fa0cd739
wgall <wojtekgall2003@gmail.com>
2023-03-16 22:38:40 +0100

    Updated workflows

--- scripts/main.py
+++ scripts/main.py
@@ -10,6 +10,8 @@ is_present = is_bucket_present(project)
 
 if is_present == False: 
     os.system("./bucket.sh {}".format(project))
+if is_present == True:
+    os.system("gcloud storage rm gs://{}/source_code.zip".format(project))
 
 os.system("./deploy.sh {}".format(project))
     
