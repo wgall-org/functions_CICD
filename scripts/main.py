0978b146f57ca8c633dd7a6703c07d914b43767a
wgall <wojtekgall2003@gmail.com>
2023-03-16 22:55:23 +0100

    Updated workflows

--- scripts/main.py
+++ scripts/main.py
@@ -6,12 +6,14 @@ project = sys.argv[1]
 from google.cloud import storage
 from functions import is_bucket_present
 
-is_present = is_bucket_present(project)
+is_present = is_bucket_present(project.strip())
+print(is_present);
 
-if is_present == False: 
-    os.system("./bucket.sh {}".format(project))
-if is_present == True:
-    os.system("gcloud storage rm gs://{}/source_code.zip".format(project))
 
-os.system("./deploy.sh {}".format(project))
+# if is_present == False: 
+#     os.system("./bucket.sh {}".format(project))
+# if is_present == True:
+#     os.system("gcloud storage rm gs://{}/source_code.zip".format(project))
+
+# os.system("./deploy.sh {}".format(project))
     
