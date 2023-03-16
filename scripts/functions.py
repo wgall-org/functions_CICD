6e1130cbdb1f1237d3cf45bc28bf3bc2cbf8c4b0
wgall <wojtekgall2003@gmail.com>
2023-03-16 23:16:13 +0100

    Removed credentials injection

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,7 +1,7 @@
 import os 
 
 def is_bucket_present(bname):
-    is_present = os.popen("gcloud storage buckets list | grep name | grep {} | cut -d : -f 2 | tr -d " " | wc -l").format(bname).read()
+    is_present = os.popen("gcloud storage buckets list | grep name | grep {} | cut -d : -f 2 | tr -d " " | wc -l".format(bname)).read()
     if is_present.strip() == "0":
         return False 
     else:
