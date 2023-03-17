0bbfbe44237a95f20bc8f115d19af2a93907a04a
wgall <wojtekgall2003@gmail.com>
2023-03-17 15:58:56 +0100

    Edited workflows

--- gcf/function.tf
+++ gcf/function.tf
@@ -10,9 +10,7 @@ resource "google_cloudfunctions_function" "function" {
   source_archive_object = "source_code.zip"
   trigger_http          = true
   entry_point           = "main"
-  depends_on = [
-    "${google_storage_bucket_object.archive}"
-  ]
+  depends_on = ["${google_storage_bucket_object.archive}"]
 }
 
 # IAM entry for all users to invoke the function
